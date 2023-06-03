cba6a7a3e4b76e5d0baaf08ee4dd6a73fde0a15d
mathbookpeace <mathbookpeace@gmail.com>
2023-06-03 19:40:44 +0800

    don't show error message when no video domain is found in the main page

--- lang_live_dl.go
+++ lang_live_dl.go
@@ -143,6 +143,8 @@ func downloadVideo(member *memberData, outfilePath string) bool {
 	if err != nil {
 		fmt.Printf("get video url failed, err = %v", err)
 		return false
+	} else if url == "" {
+		return false
 	}
 	resp, err := http.Get(url)
 	if err != nil {
@@ -168,7 +170,7 @@ func getVideoUrl(member *memberData) (string, error) {
 	re := regexp.MustCompile("https://[^\"]*.lv-play.com") // compile the regex
 	domain := re.FindString(content)
 	if domain == "" {
-		return "", fmt.Errorf("video domain not found")
+		return "", nil
 	}
 	return fmt.Sprintf("%v/live/%vY.flv", domain, member.Id), nil
 }
