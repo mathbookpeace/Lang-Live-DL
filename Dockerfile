38ee76bb2165ef6a2938d6d0e6123515399a1983
mathbookpeace <mathbookpeace@gmail.com>
2024-07-18 04:38:00 +0800

    add nas settings

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:22.10
+FROM amd64/ubuntu:22.10
 
 WORKDIR /lang-live-dl-app
 COPY . /lang-live-dl-app
---
+++ build_image.sh
@@ -0,0 +1,2 @@
+docker build -t lang-live-dl-image .
+docker save -o /Volumes/homes/mathbookpeace/docker/lang-live-dl-image.tar lang-live-dl-image:latest
