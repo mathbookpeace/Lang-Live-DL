64fd36d2ade64fa41f267a961c4b001a1210ca8a
mathbookpeace <mathbookpeace@gmail.com>
2024-12-24 11:17:15 +0800

    update home path

--- build_image.sh
+++ build_image.sh
@@ -1,2 +1,2 @@
 docker build -t lang-live-dl-image .
-docker save -o /Volumes/homes/mathbookpeace/docker/lang-live-dl-image.tar lang-live-dl-image:latest
+docker save -o /Volumes/home/docker/lang-live-dl-image.tar lang-live-dl-image:latest
