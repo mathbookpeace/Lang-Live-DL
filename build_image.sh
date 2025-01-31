8e35ba00b4a4a923923f644e9608cf0d4e7d0d39
mathbookpeace <mathbookpeace@gmail.com>
2025-01-31 19:46:56 +0800

    update build image script to specify the platform

--- build_image.sh
+++ build_image.sh
@@ -1,2 +1,2 @@
-docker build -t lang-live-dl-image .
+docker build -t lang-live-dl-image . --platform=linux/amd64 --no-cache
 docker save -o /Volumes/home/docker/lang-live-dl-image.tar lang-live-dl-image:latest
